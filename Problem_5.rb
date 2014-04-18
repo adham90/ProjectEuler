223037ab7a6d73269701e1879e60415559238bdb
adham eldeeb <adham_eldeeb@hotmail.com>
2014-04-19 00:25:17 +0200

    4

---
+++ Problem_5.rb
@@ -0,0 +1,23 @@
+# http://projecteuler.net/problem=5
+# Answer: 232792560
+# ---------------------------------
+
+min_num = 30
+
+def true_num n
+  (11...20).each do |i|
+    if n%i != 0
+      return false
+    end
+  end
+  return true
+end
+
+while true
+  if true_num(min_num) == true
+    p min_num
+    break
+  else
+    min_num += 30
+  end
+end
